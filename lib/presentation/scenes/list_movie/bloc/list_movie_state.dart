5b82db9f1cb8b0129a15ff8f506fe08b68db60e1
Nhung Bùi <nhungbth@carfromjapan.com>
2023-08-11 15:10:19 +0700

    add list movie

---
+++ lib/presentation/scenes/list_movie/bloc/list_movie_state.dart
@@ -0,0 +1,10 @@
+part of 'list_movie_cubit.dart';
+
+class ListMovieState extends Equatable {
+  const ListMovieState();
+
+  ListMovieState copyWith() => ListMovieState();
+
+  @override
+  List<Object?> get props => [];
+}
