dd7bf5bcfd73fd03d621a4041b74597d0f219ce5
Nhung Bùi <nhungbth@carfromjapan.com>
2023-08-11 15:09:34 +0700

    add resource

---
+++ lib/resources/colors.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  AppColors._();
+
+  static const Color ff671f = Color(0xFFFF671F);
+  static const Color f4600c = Color(0xFFF4600C);
+  static const Color f9fafb = Color(0xFFF9FAFB);
+  static const Color f042541 = Color(0xFF042541);
+}
