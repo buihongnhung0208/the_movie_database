1ccd0c9bc04441983eefd0de91c85f313ab72e18
Nhung Bùi <nhungbth@carfromjapan.com>
2023-09-06 16:45:01 +0700

    add recommendations

--- lib/presentation/scenes/detail_movie/bloc/detail_movie_cubit.dart
+++ lib/presentation/scenes/detail_movie/bloc/detail_movie_cubit.dart
@@ -2,14 +2,21 @@ import 'package:equatable/equatable.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:the_movie_database/api_service/core/authentication/detail-movie/detail_movie_params.dart';
 import 'package:the_movie_database/api_service/core/authentication/detail_movie_api_input.dart';
+import 'package:the_movie_database/api_service/core/authentication/list-movies/list_movies_params.dart';
+import 'package:the_movie_database/api_service/core/authentication/list_movies_api_input.dart';
 import 'package:the_movie_database/domain/usecases/get_detail_movie_usecase.dart';
+import 'package:the_movie_database/domain/usecases/get_list_movies_usecase.dart';
 import 'package:the_movie_database/model/movie_response_object/movie_response_object.dart';
 
 part 'detail_movie_state.dart';
 
 class DetailMovieCubit extends Cubit<DetailMovieState> {
-  DetailMovieCubit(this._getDetailMovieUseCase) : super(DetailMovieState.initial());
+  DetailMovieCubit(
+    this._getDetailMovieUseCase,
+    this._getListMoviesUseCase,
+  ) : super(DetailMovieState.initial());
   final GetDetailMovieUseCase _getDetailMovieUseCase;
+  final GetListMoviesUseCase _getListMoviesUseCase;
 
   Future<void> getDetailMovie({required String id}) async {
     emit(state.copyWith(isLoading: true));
@@ -32,7 +39,34 @@ class DetailMovieCubit extends Cubit<DetailMovieState> {
       },
     );
   }
+
+  Future<void> getListRecommendations({required String id}) async {
+    emit(
+      state.copyWith(
+        isLoadinglistRecommendations: true,
+      ),
+    );
+    final getListInput = ListMoviesAPIInput(
+      const ListMoviesParams(),
+      '/movie/$id/recommendations',
+    );
+    final listMovies = await _getListMoviesUseCase.call(getListInput);
+    listMovies.when(
+      success: (listObject) {
+        emit(
+          state.copyWith(
+            isLoadinglistRecommendations: false,
+            listRecommendations: listObject.results,
+          ),
+        );
+      },
+      failure: (networkError) {
+        emit(state.copyWith(isLoadinglistRecommendations: false));
+      },
+    );
+  }
+
   Future<void> getData(String id) async {
-    Future.wait([getDetailMovie( id: id)]);
+    Future.wait([getDetailMovie(id: id), getListRecommendations(id: id)]);
   }
 }
--- lib/presentation/scenes/detail_movie/bloc/detail_movie_state.dart
+++ lib/presentation/scenes/detail_movie/bloc/detail_movie_state.dart
@@ -1,30 +1,45 @@
 part of 'detail_movie_cubit.dart';
 
 class DetailMovieState extends Equatable {
-  const DetailMovieState({required this.isLoading, required this.detailMovie});
+  const DetailMovieState({
+    required this.isLoading,
+    required this.detailMovie,
+    required this.isLoadinglistRecommendations,
+    required this.listRecommendations,
+  });
 
   final bool isLoading;
   final MovieResponseObject detailMovie;
+  final bool isLoadinglistRecommendations;
+  final List<MovieResponseObject> listRecommendations;
 
   factory DetailMovieState.initial() {
     return const DetailMovieState(
       isLoading: false,
       detailMovie: MovieResponseObject(),
+      isLoadinglistRecommendations: false,
+      listRecommendations: [],
     );
   }
 
-  DetailMovieState copyWith({
-    bool? isLoading,
-    MovieResponseObject? detailMovie,
-  }) =>
+  DetailMovieState copyWith(
+          {bool? isLoading,
+          MovieResponseObject? detailMovie,
+          bool? isLoadinglistRecommendations,
+          List<MovieResponseObject>? listRecommendations}) =>
       DetailMovieState(
         isLoading: isLoading ?? this.isLoading,
         detailMovie: detailMovie ?? this.detailMovie,
+        isLoadinglistRecommendations:
+            isLoadinglistRecommendations ?? this.isLoadinglistRecommendations,
+        listRecommendations: listRecommendations ?? this.listRecommendations,
       );
 
   @override
   List<Object?> get props => [
-    isLoading,
-    detailMovie
-  ];
+        isLoading,
+        detailMovie,
+        isLoadinglistRecommendations,
+        listRecommendations,
+      ];
 }
